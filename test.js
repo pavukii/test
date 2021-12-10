bd2a811e551dfe1e4e2ff35b75de1214ba077246
Viktorija Isajeva <isajevav@gmail.com>
2021-12-10 17:26:46 +0200

    efwe

---
+++ test.js
@@ -0,0 +1,724 @@
+
+/*  function calc(num1, num2, op) {
+  if (op == "-") {
+    console.log(num1-num2)
+  }
+}
+
+function calc(num1, num2, op) {
+  if (op == "+") {
+    console.log(num1+num2)
+  }
+}
+
+function calc(num1, num2, op) {
+  if (op == "*") {
+    console.log(num1*num2)
+  }
+}
+
+function calc(num1, num2, op) {
+  if (op == "/") {
+    console.log(num1/num2);
+}
+}
+
+switch(op) {
+  case "+":
+    console.log(num1 + num2);
+    break; 
+  case "-":
+    console.log(num1 - num2);
+    break; 
+  default:
+    console.log("Error");
+}
+
+calc(5,5,"*");
+
+let animal = ["tiger", "rabbit", "cat", "dog", "lion"];
+console.log(animal[0]);
+
+
+animal.forEach(function(item){ 
+  console.log(item + " Nice one"); 
+} );
+
+// или
+
+let counter = animal.length; 
+console.log(counter); 
+
+for(let i = 0; i < counter - 1; i++){
+  console.log(animal[i]);
+}
+
+let number = 0; 
+while (number <counter - 1) {
+  console.log(animal[number]);
+  number++;
+  //number = number + 1;  
+}
+
+let text = "Hello world"; 
+console.log(text.length); 
+
+const cars = 'Toyota, Moskvich, Mustang, BMW';
+const result = cars.split(", ");
+console.log(result); 
+
+const result2 = cars.replace("M", "S");
+console.log(result2); 
+
+const result3 = cars.replaceAll("M", "S"); 
+console.log(result3); // работает не во всех браузерах
+
+function replaceText(string) {
+  return string + " Hello"; 
+}
+let result5 = replaceText("Martinsh"); 
+console.log(result5); */
+
+
+const newList = 
+   [ {
+		"guid": "E97AC152-34A1-BE6B-BA89-5DCACB7643EC",
+		"e-mail": "augue.malesuada@yahoo.couk",
+		"country": "11:03 AM",
+		"region": "Feb 11, 2022"
+	},
+	{
+		"guid": "93A42B12-2AC4-8AC8-58A0-45C3A9672278",
+		"e-mail": "integer@google.edu",
+		"country": "6:48 PM",
+		"region": "Apr 14, 2022"
+	},
+	{
+		"guid": "26ED64E6-78D7-172D-540A-5117230B4239",
+		"e-mail": "purus.sapien.gravida@protonmail.net",
+		"country": "4:59 AM",
+		"region": "Jun 3, 2021"
+	},
+	{
+		"guid": "5E4C0372-D89C-EB87-7726-A6BA0556AEE6",
+		"e-mail": "aenean.eget.metus@aol.net",
+		"country": "4:53 PM",
+		"region": "Sep 10, 2021"
+	},
+	{
+		"guid": "EECC8649-BBD6-ACEB-964C-622936910C71",
+		"e-mail": "pulvinar.arcu@outlook.org",
+		"country": "1:06 AM",
+		"region": "Sep 26, 2021"
+	},
+	{
+		"guid": "AC69E633-6EB6-5A1A-D14B-97A98D37BFEE",
+		"e-mail": "quisque.varius@outlook.com",
+		"country": "3:50 AM",
+		"region": "Jun 3, 2021"
+	},
+	{
+		"guid": "993A8146-E1C1-270A-FA79-38671D9B9859",
+		"e-mail": "pede.et@google.couk",
+		"country": "10:54 AM",
+		"region": "Nov 20, 2021"
+	},
+	{
+		"guid": "C20DB9A0-3D87-8E53-CAD1-86444AF4411C",
+		"e-mail": "at@protonmail.couk",
+		"country": "4:03 PM",
+		"region": "Jan 29, 2022"
+	},
+	{
+		"guid": "56A30C46-A4AE-06AB-51F9-232B3B79855A",
+		"e-mail": "in.faucibus.morbi@outlook.com",
+		"country": "9:20 PM",
+		"region": "May 29, 2022"
+	},
+	{
+		"guid": "9D0A5AAD-363D-8E8B-2120-2DE73C211B64",
+		"e-mail": "non@protonmail.couk",
+		"country": "5:55 AM",
+		"region": "Oct 20, 2021"
+	},
+	{
+		"guid": "8C8BB37C-0C7E-78A0-AA52-BA69CB6CF14A",
+		"e-mail": "praesent.luctus.curabitur@yahoo.net",
+		"country": "11:37 AM",
+		"region": "Jan 5, 2022"
+	},
+	{
+		"guid": "123B1B31-ED57-E964-354D-1C6A98A68ECB",
+		"e-mail": "ac.feugiat@outlook.net",
+		"country": "4:06 PM",
+		"region": "Sep 6, 2021"
+	},
+	{
+		"guid": "AE239CA9-097A-A987-9810-E4E85D5CC95E",
+		"e-mail": "sapien.cras@icloud.couk",
+		"country": "2:38 AM",
+		"region": "Jul 12, 2021"
+	},
+	{
+		"guid": "4ED1C7BB-80A0-2113-BBC5-266238A84897",
+		"e-mail": "lacinia.at@protonmail.com",
+		"country": "3:58 AM",
+		"region": "Mar 23, 2022"
+	},
+	{
+		"guid": "3BD3CE11-7E1D-D7DD-4810-CE741F354138",
+		"e-mail": "vitae@outlook.edu",
+		"country": "11:19 PM",
+		"region": "Nov 14, 2021"
+	},
+	{
+		"guid": "C693BE8D-79CE-ADEF-5ACB-6DA8D125794D",
+		"e-mail": "purus.duis@aol.couk",
+		"country": "1:00 AM",
+		"region": "Dec 12, 2020"
+	},
+	{
+		"guid": "83B2224A-75B1-89C5-A5E3-0CE4B0598E46",
+		"e-mail": "orci.donec@protonmail.edu",
+		"country": "7:01 AM",
+		"region": "Nov 2, 2022"
+	},
+	{
+		"guid": "E6EE7279-CE5E-B887-4872-90E2E7B3B754",
+		"e-mail": "non.lobortis@hotmail.net",
+		"country": "2:08 AM",
+		"region": "Aug 8, 2022"
+	},
+	{
+		"guid": "0EE53D29-E74B-0C7D-DBB4-6253125BE6C6",
+		"e-mail": "elit@outlook.ca",
+		"country": "12:01 PM",
+		"region": "Apr 10, 2022"
+	},
+	{
+		"guid": "9119031C-55AC-677A-1839-3AB1132EC83A",
+		"e-mail": "pretium.neque.morbi@icloud.ca",
+		"country": "10:41 AM",
+		"region": "Jul 22, 2022"
+	},
+	{
+		"guid": "6C3E2147-B3AA-4512-E64A-5356A9FDA8DC",
+		"e-mail": "ultrices.sit@outlook.net",
+		"country": "2:57 PM",
+		"region": "Feb 26, 2021"
+	},
+	{
+		"guid": "937BDE19-D457-65B4-8FF7-4233DD842D5C",
+		"e-mail": "mollis.duis.sit@aol.net",
+		"country": "12:05 PM",
+		"region": "Jul 5, 2022"
+	},
+	{
+		"guid": "E08541E2-CD3C-8969-3512-14D5A8D61B5D",
+		"e-mail": "ac.risus@google.couk",
+		"country": "9:12 PM",
+		"region": "Sep 15, 2021"
+	},
+	{
+		"guid": "980A6EA0-856E-044D-30E3-C348D61BA29B",
+		"e-mail": "tincidunt.pede.ac@google.ca",
+		"country": "9:53 AM",
+		"region": "May 31, 2021"
+	},
+	{
+		"guid": "6E8E4A08-A71B-7DDF-343C-AEFF946DACE6",
+		"e-mail": "eu@hotmail.edu",
+		"country": "3:46 PM",
+		"region": "Jun 27, 2021"
+	},
+	{
+		"guid": "C5CB14C3-B3A8-C774-BA56-8057A896ED98",
+		"e-mail": "non.enim@icloud.edu",
+		"country": "1:47 PM",
+		"region": "Jul 28, 2021"
+	},
+	{
+		"guid": "1E4E49A0-C2C8-88CB-D57A-A2A68638231B",
+		"e-mail": "orci.tincidunt@hotmail.edu",
+		"country": "3:07 PM",
+		"region": "Oct 14, 2021"
+	},
+	{
+		"guid": "35293AD4-7916-0973-D58A-755151565888",
+		"e-mail": "duis.cursus@icloud.edu",
+		"country": "11:05 PM",
+		"region": "Jan 8, 2021"
+	},
+	{
+		"guid": "D79EE857-52C0-6EE8-C167-7C791D79B174",
+		"e-mail": "mauris@outlook.org",
+		"country": "3:37 PM",
+		"region": "Aug 21, 2022"
+	},
+	{
+		"guid": "9AB4C36F-DAA5-6471-2A26-4BE6CF294863",
+		"e-mail": "eu.tellus@protonmail.edu",
+		"country": "8:32 PM",
+		"region": "Oct 20, 2022"
+	},
+	{
+		"guid": "89F93A28-08AC-67BB-D2C3-86413B5BD99C",
+		"e-mail": "non@hotmail.couk",
+		"country": "9:42 PM",
+		"region": "Jun 17, 2022"
+	},
+	{
+		"guid": "6195B778-548E-37D7-9989-4E346C110BC5",
+		"e-mail": "suspendisse.tristique@hotmail.com",
+		"country": "3:30 PM",
+		"region": "Feb 15, 2022"
+	},
+	{
+		"guid": "E875AA9D-A325-A91E-4818-0CACCE3331EE",
+		"e-mail": "sem.mollis@protonmail.couk",
+		"country": "12:22 PM",
+		"region": "Jul 24, 2021"
+	},
+	{
+		"guid": "543A22ED-9EA7-388F-4D84-E6F5AF337EA7",
+		"e-mail": "dis@hotmail.org",
+		"country": "3:58 AM",
+		"region": "Jan 26, 2021"
+	},
+	{
+		"guid": "6A247213-363E-D4E4-77C4-ED9BEB1181F2",
+		"e-mail": "parturient.montes@google.edu",
+		"country": "7:24 PM",
+		"region": "Jul 3, 2022"
+	},
+	{
+		"guid": "B69ECCBD-B053-9379-19BB-6958D9A1584C",
+		"e-mail": "quisque@google.ca",
+		"country": "3:41 PM",
+		"region": "Jul 26, 2021"
+	},
+	{
+		"guid": "EEE28A45-E581-5EDC-2F21-7378D128382F",
+		"e-mail": "ante@protonmail.com",
+		"country": "4:30 AM",
+		"region": "Apr 1, 2022"
+	},
+	{
+		"guid": "CCAB8CAB-0B5E-561D-B927-319C2D25A4DE",
+		"e-mail": "sed.id@google.org",
+		"country": "10:31 PM",
+		"region": "Aug 3, 2021"
+	},
+	{
+		"guid": "E2FC595E-0E54-5E60-7AB3-26E27DDF9E81",
+		"e-mail": "velit.eget@yahoo.ca",
+		"country": "4:11 PM",
+		"region": "Dec 25, 2021"
+	},
+	{
+		"guid": "680833C5-A350-9191-3C9B-3977CB576BD5",
+		"e-mail": "consequat.lectus@protonmail.org",
+		"country": "11:02 AM",
+		"region": "Jan 15, 2021"
+	},
+	{
+		"guid": "8A7A14C8-7793-6A3A-C58F-22535C87DE66",
+		"e-mail": "commodo@google.ca",
+		"country": "4:15 AM",
+		"region": "Aug 18, 2021"
+	},
+	{
+		"guid": "CBE3568A-9FDE-6C0B-ABE3-12A793646A94",
+		"e-mail": "lacus.quisque.imperdiet@icloud.com",
+		"country": "12:55 AM",
+		"region": "Jul 29, 2022"
+	},
+	{
+		"guid": "9B780E37-A43A-AC8B-1ADA-A515F2159C1F",
+		"e-mail": "posuere@google.net",
+		"country": "12:52 AM",
+		"region": "Oct 12, 2022"
+	},
+	{
+		"guid": "511B1F96-A769-5728-3D07-ED08F1787B6A",
+		"e-mail": "nulla.cras@hotmail.org",
+		"country": "11:24 AM",
+		"region": "Mar 20, 2021"
+	},
+	{
+		"guid": "F07B8A27-DA61-97BA-28A9-C7174F0D7763",
+		"e-mail": "et@protonmail.edu",
+		"country": "11:09 PM",
+		"region": "Jun 16, 2022"
+	},
+	{
+		"guid": "B62D5DCE-CC2D-E36D-5785-76A83EC160B5",
+		"e-mail": "nec.luctus@google.ca",
+		"country": "7:07 PM",
+		"region": "Dec 9, 2021"
+	},
+	{
+		"guid": "8F62E758-BCD1-1AC3-2737-7104DDC5B3AB",
+		"e-mail": "mi.pede.nonummy@hotmail.org",
+		"country": "1:48 PM",
+		"region": "Oct 24, 2022"
+	},
+	{
+		"guid": "7DED9DC4-C33E-188E-11B2-C5EBF246A42B",
+		"e-mail": "consequat@outlook.couk",
+		"country": "5:34 AM",
+		"region": "Dec 16, 2020"
+	},
+	{
+		"guid": "B9411382-2B8B-C3BE-AEBE-2638E33A18B3",
+		"e-mail": "felis.nulla@aol.ca",
+		"country": "1:47 PM",
+		"region": "Feb 1, 2021"
+	},
+	{
+		"guid": "D72B7EB9-D3EB-3254-E4C3-CD1A8742EE2A",
+		"e-mail": "augue@protonmail.org",
+		"country": "10:44 PM",
+		"region": "May 8, 2021"
+	},
+	{
+		"guid": "79573D48-16AC-96EE-AA57-A2F81C1A587B",
+		"e-mail": "at.augue@protonmail.couk",
+		"country": "5:43 PM",
+		"region": "Dec 17, 2020"
+	},
+	{
+		"guid": "A77D7279-A96C-99C7-BC14-9286B4F358A3",
+		"e-mail": "elit.pharetra@hotmail.edu",
+		"country": "8:58 AM",
+		"region": "Oct 16, 2021"
+	},
+	{
+		"guid": "67563A3B-7DF1-7F3B-9C66-C47A78DA4E91",
+		"e-mail": "maecenas.libero@icloud.couk",
+		"country": "12:02 AM",
+		"region": "Mar 11, 2022"
+	},
+	{
+		"guid": "C6AD7847-3F0B-A5C4-0AE6-8BC64E2D89C6",
+		"e-mail": "neque.sed.sem@yahoo.edu",
+		"country": "1:27 AM",
+		"region": "Sep 8, 2021"
+	},
+	{
+		"guid": "CB658B1D-8F99-A426-DAE6-DA8440D2DCE8",
+		"e-mail": "eu.tellus@yahoo.ca",
+		"country": "12:55 PM",
+		"region": "Mar 30, 2022"
+	},
+	{
+		"guid": "85C44946-887B-5254-75FD-D28CC4859BF6",
+		"e-mail": "orci@outlook.couk",
+		"country": "1:55 PM",
+		"region": "Jun 25, 2021"
+	},
+	{
+		"guid": "5D9B6E12-F1C5-966D-717B-47C1AB6829E9",
+		"e-mail": "eleifend.nunc@hotmail.net",
+		"country": "9:34 PM",
+		"region": "Feb 17, 2021"
+	},
+	{
+		"guid": "DB566EA8-E2D6-1B62-8CC2-C3825B6FA316",
+		"e-mail": "dignissim.pharetra@hotmail.net",
+		"country": "3:30 PM",
+		"region": "Jan 1, 2022"
+	},
+	{
+		"guid": "C185D68D-932D-4668-56CE-26198D73434C",
+		"e-mail": "est.ac@yahoo.net",
+		"country": "1:08 PM",
+		"region": "Sep 5, 2021"
+	},
+	{
+		"guid": "8829107F-985F-BDDB-4206-1E37C63F872E",
+		"e-mail": "justo@aol.com",
+		"country": "6:58 PM",
+		"region": "Aug 26, 2021"
+	},
+	{
+		"guid": "F15D90AC-DC73-22C1-CC42-E1143D9321DA",
+		"e-mail": "dictum.phasellus.in@protonmail.com",
+		"country": "2:15 AM",
+		"region": "Sep 11, 2021"
+	},
+	{
+		"guid": "543E0E4E-F879-2848-02AD-AA28BDD3FB07",
+		"e-mail": "vitae.nibh@aol.edu",
+		"country": "6:30 AM",
+		"region": "Jan 12, 2022"
+	},
+	{
+		"guid": "4B15939C-44A3-2008-DE6B-DA8CCB46096C",
+		"e-mail": "dis.parturient@yahoo.org",
+		"country": "6:16 AM",
+		"region": "Oct 30, 2022"
+	},
+	{
+		"guid": "CB8BCBA8-7979-71CC-B15A-363D4363A93C",
+		"e-mail": "eu@protonmail.couk",
+		"country": "1:33 PM",
+		"region": "Sep 14, 2022"
+	},
+	{
+		"guid": "C8BEBB97-975E-7FC4-1AC8-484B8AE787AC",
+		"e-mail": "adipiscing.mauris@outlook.edu",
+		"country": "2:40 PM",
+		"region": "May 5, 2022"
+	},
+	{
+		"guid": "BAF3FBF0-5BB6-2D6D-B9C7-0C36A4BEA78F",
+		"e-mail": "fusce.mi.lorem@protonmail.net",
+		"country": "7:36 AM",
+		"region": "Oct 21, 2022"
+	},
+	{
+		"guid": "D52EF25A-3BA8-8446-6E56-C3E25D36AD1D",
+		"e-mail": "mi.ac@icloud.net",
+		"country": "11:20 PM",
+		"region": "Feb 28, 2022"
+	},
+	{
+		"guid": "0E35B8B5-E12B-636C-6B02-3D3DD7B29AA4",
+		"e-mail": "gravida.nunc@icloud.org",
+		"country": "7:13 PM",
+		"region": "Nov 25, 2022"
+	},
+	{
+		"guid": "EF849703-D0D2-78DD-296C-2C81181BA2D6",
+		"e-mail": "dolor.quam@google.ca",
+		"country": "11:10 PM",
+		"region": "Aug 18, 2022"
+	},
+	{
+		"guid": "602DE4BE-7B7D-3B3E-D589-9C434C1B2131",
+		"e-mail": "in.sodales@icloud.org",
+		"country": "1:53 AM",
+		"region": "Jan 17, 2021"
+	},
+	{
+		"guid": "B3B3D42C-A9FC-36D4-CCA7-2B2B8886EDC3",
+		"e-mail": "sed@icloud.couk",
+		"country": "9:07 AM",
+		"region": "May 15, 2021"
+	},
+	{
+		"guid": "E304A4A7-5957-118D-C8AE-22AC613FA59F",
+		"e-mail": "dignissim.maecenas@yahoo.com",
+		"country": "9:09 PM",
+		"region": "Feb 19, 2022"
+	},
+	{
+		"guid": "9F77D836-AC2E-D10A-EA08-498FD95FE71E",
+		"e-mail": "et.malesuada.fames@aol.edu",
+		"country": "9:05 AM",
+		"region": "Jul 8, 2022"
+	},
+	{
+		"guid": "140EA03A-339E-F3EE-7816-AC45A37EC793",
+		"e-mail": "augue.ac.ipsum@aol.net",
+		"country": "9:48 AM",
+		"region": "Sep 21, 2022"
+	},
+	{
+		"guid": "D53CCACD-2752-B131-6952-009C34A771AB",
+		"e-mail": "montes.nascetur@icloud.net",
+		"country": "8:04 AM",
+		"region": "Feb 2, 2022"
+	},
+	{
+		"guid": "CB771BEA-1C39-561D-B2A5-C611636E83FE",
+		"e-mail": "vel.pede@icloud.edu",
+		"country": "3:47 AM",
+		"region": "Feb 6, 2022"
+	},
+	{
+		"guid": "DA7AEDDE-E2AF-B4B0-DC57-530341D31543",
+		"e-mail": "ut.eros@outlook.couk",
+		"country": "10:53 PM",
+		"region": "Mar 14, 2022"
+	},
+	{
+		"guid": "59DD3643-CB4C-CD42-1C8A-D44CCC4FD4DC",
+		"e-mail": "sem.molestie.sodales@icloud.org",
+		"country": "4:02 PM",
+		"region": "Jul 11, 2021"
+	},
+	{
+		"guid": "5176B601-729F-3643-7668-3F89E9B1B725",
+		"e-mail": "orci@outlook.edu",
+		"country": "7:13 PM",
+		"region": "Nov 21, 2021"
+	},
+	{
+		"guid": "A9CC8289-255E-D97F-2293-E506EB3C09A9",
+		"e-mail": "phasellus.elit@outlook.net",
+		"country": "9:46 AM",
+		"region": "Jan 23, 2022"
+	},
+	{
+		"guid": "E449B14A-DAA1-0DEE-5660-E3953485D36E",
+		"e-mail": "risus.odio@protonmail.couk",
+		"country": "7:17 PM",
+		"region": "Jul 15, 2021"
+	},
+	{
+		"guid": "466652E7-433E-3748-F8AE-643D37E2B1E5",
+		"e-mail": "mus.proin@icloud.com",
+		"country": "2:42 AM",
+		"region": "Feb 24, 2022"
+	},
+	{
+		"guid": "121FDDD9-2E89-FC37-5583-24E7E6A01F47",
+		"e-mail": "cras@aol.couk",
+		"country": "7:40 PM",
+		"region": "Oct 31, 2022"
+	},
+	{
+		"guid": "EB6AA075-509D-0E4D-BE7D-59341E1D41CD",
+		"e-mail": "sodales@protonmail.ca",
+		"country": "7:01 PM",
+		"region": "Sep 13, 2021"
+	},
+	{
+		"guid": "3737C6B1-E5C5-6B6C-57F2-316561D3B2E8",
+		"e-mail": "ac.mattis@yahoo.com",
+		"country": "10:28 PM",
+		"region": "Nov 13, 2022"
+	},
+	{
+		"guid": "23A1B97D-946B-B2B2-E889-345951799C6E",
+		"e-mail": "iaculis.aliquet@aol.ca",
+		"country": "3:24 PM",
+		"region": "Jun 27, 2021"
+	},
+	{
+		"guid": "9BA775B6-B9C8-7094-B4B8-AD72AB64A43B",
+		"e-mail": "fringilla@outlook.couk",
+		"country": "8:32 PM",
+		"region": "Jun 6, 2022"
+	},
+	{
+		"guid": "76203CEC-C72C-3531-58ED-D4AFD0133A55",
+		"e-mail": "neque@outlook.ca",
+		"country": "11:22 PM",
+		"region": "Jan 27, 2021"
+	},
+	{
+		"guid": "171A9E65-35C8-34EA-0A1C-5DE6820CFE6D",
+		"e-mail": "habitant@google.edu",
+		"country": "4:16 AM",
+		"region": "May 14, 2021"
+	},
+	{
+		"guid": "23A53BD7-7262-CB7C-72CE-6FC2E405E3ED",
+		"e-mail": "aliquam@protonmail.org",
+		"country": "2:57 AM",
+		"region": "Jun 13, 2021"
+	},
+	{
+		"guid": "AE953D38-3B90-F82A-13A1-5B15C73A2B6E",
+		"e-mail": "integer.eu@hotmail.com",
+		"country": "8:02 PM",
+		"region": "Nov 19, 2021"
+	},
+	{
+		"guid": "33DAC237-E947-E73F-9D20-BB25E69DDF02",
+		"e-mail": "aenean.massa@outlook.com",
+		"country": "7:48 PM",
+		"region": "Feb 16, 2021"
+	},
+	{
+		"guid": "04E2175D-E2DC-19DB-4BA7-D0A7A4CE4F87",
+		"e-mail": "egestas@protonmail.edu",
+		"country": "6:10 AM",
+		"region": "Mar 16, 2021"
+	},
+	{
+		"guid": "C1F40A43-85D6-CD3C-9EE9-3895BB6C945D",
+		"e-mail": "donec@yahoo.com",
+		"country": "2:27 AM",
+		"region": "Jun 7, 2022"
+	},
+	{
+		"guid": "B7B19F34-8BD6-966F-6E9B-8985A8856158",
+		"e-mail": "arcu.eu@hotmail.couk",
+		"country": "2:23 PM",
+		"region": "Oct 5, 2021"
+	},
+	{
+		"guid": "84CF9BE5-348D-D99E-E2E7-A28C959E7358",
+		"e-mail": "elit.fermentum@icloud.ca",
+		"country": "4:50 AM",
+		"region": "Sep 8, 2021"
+	},
+	{
+		"guid": "3E647C5D-A68F-42FC-ADC9-8BCB487FB8FB",
+		"e-mail": "donec.est@icloud.net",
+		"country": "4:33 PM",
+		"region": "Apr 5, 2021"
+	},
+	{
+		"guid": "405B3A67-B7CB-32DB-262C-293843777D43",
+		"e-mail": "aliquam.nisl@icloud.couk",
+		"country": "3:46 PM",
+		"region": "Oct 14, 2021"
+	},
+	{
+		"guid": "48CC169A-1C2C-2EBE-4B84-1D134A1B5728",
+		"e-mail": "vel.vulputate@yahoo.org",
+		"country": "9:11 PM",
+		"region": "Nov 14, 2021"
+	},
+	{
+		"guid": "CE352396-1A5D-5DB6-99BE-4F34F13FF87A",
+		"e-mail": "duis.mi.enim@hotmail.ca",
+		"country": "7:21 PM",
+		"region": "Feb 9, 2022"
+	}];
+
+console.log(newList); 
+
+let counter2 = newList.length; 
+console.log(counter2);
+
+
+const {"e-mail": email} = newList;
+console.log(email);   
+
+var results = [];
+var search = ".com";
+for(var i=0; i<newList.length; i++) { // где const newText - куда скопирован массив
+  for(key in newList[i]) {
+    if(newList[i][key].indexOf(search)!=-1) {
+      results.push(newList[i]);
+    }
+  }
+}
+
+console.log(results);
+
+/*const newArray = newList.filter(function (element) {
+return (element.region === "Nov 20, 2021");
+});
+console.log(newArray); 
+
+const newArray2 = newList.filter(function (element) {
+  return (element["e-mail"].length > 25);
+});
+console.log(newArray2) */
+
+const emailCom = newList.filter(function (element) {
+  return (element["e-mail"].slice(-3) === "com");
+});
+console.log(emailCom)
+
+const regSep = newList.filter(function (element) {
+  return (element["region"].slice(0,3) === "Sep");
+});
+console.log(regSep)
